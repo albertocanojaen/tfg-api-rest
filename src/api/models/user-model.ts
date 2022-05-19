8397d24d48671ef04a6c03e20b934600fafa82f2
Alberto <albertocanojaen@hotmail.com>
2022-05-19 16:44:18 +0200

    Change the user model to transform from Prisma and to Prisma

--- src/api/models/user-model.ts
+++ src/api/models/user-model.ts
@@ -1,12 +1,12 @@
 import { Passwords } from '../../lib/password';
 import { User } from '@prisma/client';
 
-export class UserModel implements User {
-    public id: number;
-    public name: string;
-    public password: string;
-    public email: string;
-    public createdAt: Date;
+export class UserModel {
+    public id?: number;
+    public name?: string | null;
+    public password?: string;
+    public email?: string;
+    public createdAt?: Date;
 
     /**
      * Class constructor
@@ -15,10 +15,33 @@ export class UserModel implements User {
      */
     constructor(args: User) {
         this.id = args?.id;
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        this.name = args.name!;
-        this.password = Passwords.encrypt(args?.password);
-        this.email = args?.email;
-        this.createdAt = new Date();
+        this.name = args?.name ? args.name : '';
+        this.password = args?.password ? Passwords.encrypt(args.password) : '';
+        this.email = args?.email ? args.email : '';
+        this.createdAt = args?.createdAt ? args.createdAt : new Date();
+    }
+
+    public fromModelToPrisma() {
+        return {
+            id: this.id!,
+            name: this.name!,
+            email: this.email!,
+            password: this.password!,
+            createdAt: this.createdAt!,
+        };
+    }
+
+    public static fromPrismaToModel(args: User | null): UserModel | null {
+        if (args) {
+            return new UserModel({
+                id: args?.id,
+                name: args?.name,
+                email: args?.email,
+                password: args?.password,
+                createdAt: args?.createdAt,
+            });
+        }
+
+        return null;
     }
 }
