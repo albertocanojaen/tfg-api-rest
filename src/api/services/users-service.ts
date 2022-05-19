b390d3148b2dc38f6e2fc903b20e2bac9ca1f236
Alberto <albertocanojaen@hotmail.com>
2022-05-19 16:46:35 +0200

    Adapt the user service to the interface

--- src/api/services/users-service.ts
+++ src/api/services/users-service.ts
@@ -1,24 +1,71 @@
 import PrismaHandler from '../../lib/prisma-handler';
 import { User } from '@prisma/client';
 import { CRUD } from '../../interfaces/service';
+import { Criteria } from '../../lib/criteria/criteria';
 
 class UsersService implements CRUD<User> {
+    /**
+     * Create a new user in the database
+     *
+     * @param args
+     */
     public async create(args: User): Promise<User> {
-        console.log(args);
-
         return await PrismaHandler.client.user.create({
             data: args,
         });
     }
 
-    public async read(queryParameters: any): Promise<User[]> {
-        return await PrismaHandler.client.user.findMany(queryParameters);
+    /**
+     * Get the users from the database by criteria.
+     *
+     * @param queryParameters
+     */
+    public async getByCriteria(queryParameters: Criteria): Promise<User[]> {
+        // Generate the Prisma query parameters
+        let prismaQueryParameters = {};
+
+        // If the parameters are not empty
+        if (queryParameters) {
+            // Get the parameters readable for Prisma
+            prismaQueryParameters = queryParameters.parseCriteriaToPrisma();
+        }
+
+        // Make the query
+        return await PrismaHandler.client.user.findMany(prismaQueryParameters);
     }
 
-    update(modifiedObject: User): Promise<void> {
-        throw new Error('Method not implemented.');
+    /**
+     * Get an user from the database by id.
+     *
+     * @param id
+     */
+    public async getById(id: number): Promise<User | null> {
+        return await PrismaHandler.client.user.findUnique({
+            where: {
+                id: id,
+            },
+        });
     }
 
+    /**
+     * Update a user from the database.
+     *
+     * @param id
+     */
+    async update(modifiedObject: User): Promise<User> {
+        return await PrismaHandler.client.user.update({
+            where: {
+                id: modifiedObject.id,
+            },
+            data: modifiedObject,
+        });
+    }
+
+    /**
+     * Delete a user from the database by id
+     * @param id
+     * @returns
+     */
     public async delete(id: number): Promise<User> {
         return await PrismaHandler.client.user.delete({
             where: {
